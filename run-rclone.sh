10d7f5ec81da2389081a437d0f617ed399a848e4
Clayton O'Neill <claytono@github.com>
2018-05-21 06:51:34 -0400

    Use config file in writable directory
    Kubernetes 1.9.6 and higher use read-only config maps and for some
    reason rclone always wants to rewrite its config.

--- run-rclone.sh
+++ run-rclone.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -eu
 
-# This only wrapper only exists for backwards compatibility and
-# rclone-wrapper.rb should be used directly for anything new.
 set -o pipefail
 
-./rclone-wrapper.rb "$@" 2>&1 \
-    | tee log \
+cp ~/.config/rclone/rclone.conf /tmp
+./rclone-wrapper.rb --config /tmp/rclone.conf "$@"
 
